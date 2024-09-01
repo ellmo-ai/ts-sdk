832dce00c2c496430b4ea6757314659abe4660ac
Mufeez Amjad <mufeez.amjad@outlook.com>
2024-09-01 15:32:38 -0400

    Execute tests from the CLI

--- src/test/index.ts
+++ src/test/index.ts
@@ -8,6 +8,7 @@ export class Test<T> {
         this.func = (input: T) => {
             const result = func(input);
             console.log(`Test ${id} returned ${result}`);
+            return
         };
     }
 
